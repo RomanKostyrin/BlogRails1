512d70327f12ee6ec36191e553ee2040e7b36797
RomanKostyrin <enemy-iubip@mail.ru>
2019-06-28 19:08:34 +0300

    add validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,8 @@
 class Contact < ApplicationRecord
+  validates :email, presence: true
+  validates :email, length: {maximum: 35}
+  validates :email, length: {minimum: 5}
+  validates :message, presence: true
+  validates :message, length: {maximum: 500}
+
 end
