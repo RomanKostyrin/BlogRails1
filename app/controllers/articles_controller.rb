1183e5c7e45c1adf1aee920bdbafb2e34effc8e5
RomanKostyrin <enemy-iubip@mail.ru>
2019-06-29 12:31:18 +0300

    added edit articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
 # <%= link_to 'Show all articles', articles_path %>
   
+
+
   def index
     @articles =Article.all
   end
@@ -14,13 +16,26 @@ class ArticlesController < ApplicationController
  
   def create
       @article = Article.new(article_params)
-    if @article.valid?
-      @article.save
+    if @article.save
       redirect_to @article
     else
       render action: 'new'
     end
     # render plain: params[:article].inspect
+  end
+
+  def edit
+    @article =Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else 
+      render action: 'edit'
+    end
   end
 
    private
